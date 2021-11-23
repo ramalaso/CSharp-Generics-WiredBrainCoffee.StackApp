3c33131b7f04cb2657b411639b4e6238f254c025
Raul.Laredo <Raul.Laredo@niceincontact.com>
2021-11-23 10:16:25 -0400

    Added T Method

--- WiredBrainCoffee.StackApp/Program.cs
+++ WiredBrainCoffee.StackApp/Program.cs
@@ -6,7 +6,28 @@ namespace WiredBrainCoffee.StackApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StackDoubles();
+        }
+
+        private static void StackDoubles()
+        {
+            var stack = new SimpleStack<double>();
+            stack.Push(1.2);
+            stack.Push(3.2);
+            stack.Push(4.2);
+            stack.Push(1.8);
+            stack.Push(3.3);
+
+            double sum = 0.0;
+
+            while (stack.Count > 0)
+            {
+                double item = stack.Pop();
+                Console.WriteLine($"Item: {item}");
+                sum += item;
+            }
+
+            Console.WriteLine($"Sum: {sum}");
         }
     }
 }
